983cfa7757ab51007055d8fa2fe39de363f92964
AnantKrishna <anant2004.1@gmail.com>
2026-07-23 15:15:40 +0530

    implement user registration and JWT authentication; add new controllers and routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,17 +3,18 @@ import express from "express";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"; 
+import { app } from "./app.js";
 
 
 dotenv.config({
-    path : './env'
+    path : './.env'
 })
 
 
 connectDB()
 .then(() => {
 
-    app.on((error) => {
+    app.on("error",(error) => {
         console.log("EXPRESS not able to talk with the MONGO DB",error);
         throw error 
 
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -48,10 +48,10 @@ const vserSchema = new Schema(
     },{timestamps : true}
 )
 
-userSchema.pre("save", function(next) {
+userSchema.pre("save", async function(next) {
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password,19);
+    this.password = await bcrypt.hash(this.password,10);
     next()
 })
 
